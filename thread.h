4adbd7852fba631dc9a4d39ad736ccf1a5406545
NZT <nikolaztodorovic26@gmail.com>
2019-07-09 13:20:51 +0200

    Thread.h set to first version

--- thread.h
+++ thread.h
@@ -3,7 +3,7 @@
 
 typedef unsigned long StackSize;
 const StackSize defaultStackSize = 4096;
-const StackSize maxStackSize = 1UL << 16;
+//const StackSize maxStackSize = 1UL << 16;
 typedef unsigned int Time; //time, x55ms
 const Time defaultTimeSlice = 2;
 typedef int ID;
@@ -12,7 +12,7 @@ class PCB; //Kernel's implementaton of a user's thread
 
 class Thread {
 public:
-    
+
     void start();
     void waitToComplete();
     virtual ~Thread();
@@ -22,7 +22,7 @@ public:
     static Thread *getThreadById(ID id);
 
 protected:
-    
+
     friend class PCB;
     Thread (StackSize stackSize = defaultStackSize, Time timeSlice= defaultTimeSlice);
     virtual void run() {}
@@ -34,4 +34,3 @@ private:
 void dispatch();
 
 #endif
-
